b129e1d401687e59b1608023b2f024e38ce76fb3
gthaylirodriguez <gthaylirodriguez@gmail.com>
2023-07-01 10:42:51 -0500

    Protect the end points.

--- routes/api.js
+++ routes/api.js
@@ -9,9 +9,9 @@ let AuthController = require('../controllers/authController');
 
 let userProtectUrl = require('../middlewares/authUsuario').userProtectUrl;
 
-api.get("/", WelcomeController.welcome);
-api.get("/usuarios", UsuariosController.usuarios);
-api.get("/usuario/:filtro", UsuariosController.usuario);
+api.get("/", userProtectUrl,WelcomeController.welcome);
+api.get("/usuarios",userProtectUrl,  UsuariosController.usuarios);
+api.get("/usuario/:filtro",userProtectUrl,  UsuariosController.usuario);
 api.post("/usuario",userProtectUrl, [
                     body('usuario_id').not().isEmpty(),
                     body('nombre').not().isEmpty(),
@@ -19,14 +19,14 @@ api.post("/usuario",userProtectUrl, [
                     body('email').not().isEmpty(),
                     body('pass').not().isEmpty()
                 ], UsuariosController.crear_usuario);
-api.put("/usuario/:filtro",[
+api.put("/usuario/:filtro",userProtectUrl, [
                     body('nombre').not().isEmpty(),
                     body('edad').not().isEmpty(),
                     body('email').not().isEmpty(),
                     body('pass').not().isEmpty()
                 ], UsuariosController.actualizar_usuario);
 
-api.delete("/usuario/:filtro", UsuariosController.eliminar_usuario);
+api.delete("/usuario/:filtro",userProtectUrl, UsuariosController.eliminar_usuario);
 
 api.post("/login",[
     body('email').not().isEmpty(),
